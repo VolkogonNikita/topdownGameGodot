dde7dd7c872b87287c92fff5d381761317f5bfcf
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2025-08-12 18:32:04 +0300

    123

--- scripts/new_script.gd
+++ scripts/new_script.gd
@@ -1,12 +1,20 @@
 extends Node
 
-var hello
+var PLAYER_STATE = "idle"
 
 func _ready() -> void:
+	print("1")
 	pass
 
 func _physics_process(delta: float) -> void:
-	pass
+	russian_rap()
+	print(1)
+
+func _process(delta: float) -> void:
+	russian_rap()
+	print(2)
 
 func russian_rap():
-	pass
+	if PLAYER_STATE == "idle":
+		$AnimatedSprite2D.play("idle")
+	print(1)
