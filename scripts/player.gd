ca55f0a789a014ae4b5dde43567f1f718844ce68
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2025-08-13 17:48:16 +0300

    added enemy skeleton and harassment

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var SPEED = 200
 var PLAYER_STATE = "idle"
 
+func method_player():
+	pass
+
 func _process(delta: float) -> void:
 	russian_rap()
 	var direction = movement_vector().normalized()
@@ -12,8 +15,12 @@ func _process(delta: float) -> void:
 func movement_vector():
 	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var movement_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	PLAYER_STATE = ""
 	return Vector2(movement_x, movement_y)
 	
 func russian_rap():
 	if PLAYER_STATE == "idle":
 		$AnimatedSprite2D.play("idle")
+	##################
+	else: 
+		$AnimatedSprite2D.play("run")
