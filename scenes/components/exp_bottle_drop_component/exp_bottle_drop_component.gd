21740d1cec33371b31c739242f36a28e9c1efdc0
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2025-10-01 09:59:11 +0300

    выпадение(с шансом) + сбор бутылок с опытом, добавление hp скелетам

---
+++ scenes/components/exp_bottle_drop_component/exp_bottle_drop_component.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@export var exp_bottle_scene: PackedScene
+@export var health_component: Node
+@export var drop_percent = .5
+
+func _ready():
+	(health_component as HeathComponent).died.connect(on_died)
+
+func on_died():
+	if randf() < drop_percent:
+		return
+	
+	if exp_bottle_scene == null:
+		return
+	
+	if not owner is Node2D:
+		return
+		
+	var spawn_pos = (owner as Node2D).global_position
+	var exp_bottle_instance = exp_bottle_scene.instantiate() as Node2D
+	owner.get_parent().add_child(exp_bottle_instance)#поднимаемся вверх по иерархии(на node level) и добавляем элемент в node level
+	exp_bottle_instance.global_position = spawn_pos
