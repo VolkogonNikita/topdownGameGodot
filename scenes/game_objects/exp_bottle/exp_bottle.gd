03c57640ed7defa19653b88b7f24bd355ac07b5f
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2025-10-01 09:59:11 +0300

    выпадение(с шансом) + сбор бутылок с опытом, добавление hp скелетам

--- scenes/game_objects/exp_bottle/exp_bottle.gd
+++ scenes/game_objects/exp_bottle/exp_bottle.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-#var player = get_tree().get_first_node_in_group("player")
+var bottle_experience = 5
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
+	Global.experience_bottle_collected.emit(bottle_experience)
 	queue_free()
-
- 
