7fca7b2a3cc51c2f1f87a902952444fc74c31a9d
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2025-12-22 01:04:41 +0300

    a lot of new features. progress bar, player health, collisions between mobs and collision to attack player

--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -1,9 +1,19 @@
 #added to diagram
 extends CharacterBody2D
 
+@onready var health_component = $HealthComponent
+@onready var grace_period = $GracePeriod
+@onready var progress_bar = $ProgressBar
+
 var SPEED = 200
 var acceleration = .15
 var PLAYER_STATE = "idle"
+var enemies_colliding = 0 #how many enemies attacks player at the moment
+
+func _ready():
+	health_component.died.connect(on_died)
+	health_component.health_changed.connect(on_health_changed)
+	health_update()
 
 func _process(_delta: float) -> void:
 	var direction = movement_vector().normalized()
@@ -32,3 +42,30 @@ func play_anim():
 	else: 
 		if PLAYER_STATE == "run":
 			$AnimatedSprite2D.play("run")
+
+func check_if_damaged():
+	if enemies_colliding == 0 || !grace_period.is_stopped():
+		return
+	health_component.take_damage(1)
+	grace_period.start()
+	
+	print(health_component.current_health)
+
+func _on_player_hurt_box_area_entered(area: Area2D) -> void:
+	enemies_colliding += 1
+	check_if_damaged()
+
+func _on_player_hurt_box_area_exited(area: Area2D) -> void:
+	enemies_colliding -= 1
+
+func on_died():
+	queue_free()
+
+func health_update():
+	progress_bar.value = health_component.get_health_value()
+
+func on_health_changed():
+	health_update()
+
+func _on_grace_period_timeout() -> void:
+	check_if_damaged()
