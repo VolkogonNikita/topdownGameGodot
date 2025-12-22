2b22361b11192510f91b6545ec4848b402afc262
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2025-12-23 01:12:04 +0300

    difficulty

--- scenes/manager/spawner/enemy_spawner.gd
+++ scenes/manager/spawner/enemy_spawner.gd
@@ -1,7 +1,24 @@
 #added to diagram
 extends Node
 
+@export var arena_time_manager: ArenaTimeManager
+
+@onready var timer = $Timer
+
 var skeleton_scene = preload("res://scenes/game_objects/enemies/skeleton/skeleton.tscn")
+
+var base_spawn_time
+var min_spawn_time = 0.2
+var difficulty_multiplier = 0.01
+
+func _ready():
+	base_spawn_time = timer.wait_time
+	arena_time_manager.difficulty_increased.connect(on_difficulty_increased)
+
+func on_difficulty_increased(difficulty_level: int):
+	var new_spawn_time = max(min_spawn_time,(base_spawn_time - difficulty_level * difficulty_multiplier))
+	timer.wait_time = new_spawn_time
+	print(timer.wait_time)
 
 func _on_timer_timeout() -> void:
 	var player = get_tree().get_first_node_in_group("player") as Node2D
