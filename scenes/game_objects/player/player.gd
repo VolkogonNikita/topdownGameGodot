a815b144bb53605324b240770abc3840225fc6b2
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-01-24 21:23:27 +0300

    axe upgarde(at first player doesn't have an axe, and if player gets new level, he can choose axe upgrade and it appears). The lesson was difficult, i need to figure it out better

--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var health_component = $HealthComponent
 @onready var grace_period = $GracePeriod
 @onready var progress_bar = $ProgressBar
+@onready var ability_manager = $AbilityManager
 
 var SPEED = 200
 var acceleration = .15
@@ -13,6 +14,7 @@ var enemies_colliding = 0 #how many enemies attacks player at the moment
 func _ready():
 	health_component.died.connect(on_died)
 	health_component.health_changed.connect(on_health_changed)
+	Global.ability_upgrade_added.connect(on_ability_upgrade_added)
 	health_update()
 
 func _process(_delta: float) -> void:
@@ -69,3 +71,10 @@ func on_health_changed():
 
 func _on_grace_period_timeout() -> void:
 	check_if_damaged()
+
+
+func on_ability_upgrade_added(upgrade:AbilityUpgrade, current_upgrades: Dictionary):
+	if not upgrade is NewAbility:
+		return 
+	var new_ability = upgrade as NewAbility
+	ability_manager.add_child(new_ability.new_ability_scene.instantiate())
--- scenes/manager/upgrade/upgrade_manager.gd
+++ scenes/manager/upgrade/upgrade_manager.gd
@@ -7,20 +7,11 @@ extends Node
 
 var current_upgrades = {}
 
+
 func _ready() -> void:
 	experience_manager.level_up.connect(on_level_up)
-	
-func on_level_up(current_level):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	 
-	if chosen_upgrade == null:
-		return
-	
-	var upgrade_sceen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
-	add_child(upgrade_sceen_instance)
-	upgrade_sceen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_sceen_instance.update_selected.connect(on_upgrade_selected)
 
+	
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
 	
@@ -28,12 +19,30 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		current_upgrades[upgrade.id] = {
 			"upgrade": upgrade,
 			"quantity": 1
-		}
-		
+		}		
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
 	
 	Global.ability_upgrade_added.emit(upgrade, current_upgrades)
 
+
+func pick_upgrades():
+	var chosen_upgrades: Array[AbilityUpgrade]
+	var pool_copy = upgrade_pool.duplicate()
+	for i in 2:
+		var chosen_upgrade = pool_copy.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		pool_copy = pool_copy.filter(func(upgrade): return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgrades
+
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+
+func on_level_up(current_level):
+	var upgrade_sceen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
+	add_child(upgrade_sceen_instance)
+	var chosen_upgrades = pick_upgrades()
+	upgrade_sceen_instance.set_ability_upgrades(chosen_upgrades) #as Array[AbilityUpgrade]
+	upgrade_sceen_instance.update_selected.connect(on_upgrade_selected)
