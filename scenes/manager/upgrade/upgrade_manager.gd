87764369536a5fc8c8de53e08a8b75ad372d250a
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-01-25 13:56:45 +0300

    max quantity for weapons upgrades

--- scenes/manager/upgrade/upgrade_manager.gd
+++ scenes/manager/upgrade/upgrade_manager.gd
@@ -24,12 +24,19 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		current_upgrades[upgrade.id]["quantity"] += 1
 	
 	Global.ability_upgrade_added.emit(upgrade, current_upgrades)
+	
+	if upgrade.max_quantity > 0:
+		var current_quantity = current_upgrades[upgrade.id]["quantity"]
+		if current_quantity == upgrade.max_quantity:
+			upgrade_pool = upgrade_pool.filter(func(upgrade_pool): return upgrade_pool.id != upgrade.id)
 
 
 func pick_upgrades():
 	var chosen_upgrades: Array[AbilityUpgrade]
 	var pool_copy = upgrade_pool.duplicate()
 	for i in 2:
+		if pool_copy.size() == 0:
+			break
 		var chosen_upgrade = pool_copy.pick_random() as AbilityUpgrade
 		chosen_upgrades.append(chosen_upgrade)
 		pool_copy = pool_copy.filter(func(upgrade): return upgrade.id != chosen_upgrade.id)
@@ -44,5 +51,5 @@ func on_level_up(current_level):
 	var upgrade_sceen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
 	add_child(upgrade_sceen_instance)
 	var chosen_upgrades = pick_upgrades()
-	upgrade_sceen_instance.set_ability_upgrades(chosen_upgrades) #as Array[AbilityUpgrade]
+	upgrade_sceen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
 	upgrade_sceen_instance.update_selected.connect(on_upgrade_selected)
