a78c61b7dfbf7ce646cc239cfebbaa3c29df665f
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-01-29 19:34:44 +0300

    fixed bad work of abilityCaraUpgrade(player couldn't click panelcontainer with NameLabel when he selected upgrade).

--- scenes/ui/upgrade_card/ability_upgrade_card.gd
+++ scenes/ui/upgrade_card/ability_upgrade_card.gd
@@ -34,16 +34,16 @@ func _on_gui_input(event: InputEvent) -> void:
 		card_selected.emit()
 
 
-func _on_panel_container_gui_input(event: InputEvent) -> void:
-	if disabled: return
-	if event.is_action_pressed("left_click"):
-		disabled = true
-		for card in get_tree().get_nodes_in_group("upgrade_card"):
-			if card == self:
-				animation_player.play("selected")
-			else: card.animation_player.play("discard")
-		await animation_player.animation_finished
-		card_selected.emit()
+#func _on_panel_container_gui_input(event: InputEvent) -> void:
+	#if disabled: return
+	#if event.is_action_pressed("left_click"):
+		#disabled = true
+		#for card in get_tree().get_nodes_in_group("upgrade_card"):
+			#if card == self:
+				#animation_player.play("selected")
+			#else: card.animation_player.play("discard")
+		#await animation_player.animation_finished
+		#card_selected.emit()
 
 
 func _on_mouse_entered() -> void:
@@ -56,11 +56,11 @@ func _on_mouse_exited() -> void:
 	scale = Vector2(1.0, 1.0)
 
 
-func _on_panel_container_mouse_entered() -> void:
-	if disabled: return
-	scale = Vector2(1.1, 1.1)
+#func _on_panel_container_mouse_entered() -> void:
+	#if disabled: return
+	#scale = Vector2(1.1, 1.1)
 
 
-func _on_panel_container_mouse_exited() -> void:
-	if disabled: return
-	scale = Vector2(1.0, 1.0)
+#func _on_panel_container_mouse_exited() -> void:
+	#if disabled: return
+	#scale = Vector2(1.0, 1.0)
