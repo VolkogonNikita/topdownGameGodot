01f6e17e5da32eb348a43e77a3fb1b6cf57d843a
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-01-30 03:02:48 +0300

    some small changes with end screen: added shadows in text in all labels and easy pretty animation by tween

--- scenes/ui/end_screen/end_screen.gd
+++ scenes/ui/end_screen/end_screen.gd
@@ -2,8 +2,20 @@ extends CanvasLayer
 class_name  EndScreen
 
 @onready var game_result_label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/GameResultLabel
+@onready var panel_container = $MarginContainer/PanelContainer
 
 func _ready() -> void:
+	#we do it because pivot is in left top corner by default 
+	#but we need the pivot to be in the center 
+	#pivot offset and size have Vector2 type, that's why we 
+	#use only one string to calculate position
+	#(the engine know that it needs to calculate 2 veriables) 
+	panel_container.pivot_offset = panel_container.size / 2 
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	#Vector2.ONE because it's original scale (x=1, y=1)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .6)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	get_tree().paused = true
 
 func change_to_victory():
