7489c6439d837c47c58145f6f4ac0de0f8cb9e8b
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-01-31 21:12:57 +0300

    downloaded a lot of sounds. use 10 of them for hit and death(5 for each), but only for enemies now, mat be in the next commit we'll use them for player too

--- scenes/components/hurt_box_component/hurt_box_component.gd
+++ scenes/components/hurt_box_component/hurt_box_component.gd
@@ -3,6 +3,7 @@ extends Area2D
 class_name HurtBoxComponent
 
 @export var heath_component: HeathComponent
+@export var hit_sound_component: AudioStreamPlayer2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if not area is HitBoxComponent:
@@ -13,3 +14,4 @@ func _on_area_entered(area: Area2D) -> void:
 		
 	var hit_box_component = area as HitBoxComponent
 	heath_component.take_damage(hit_box_component.damage)
+	hit_sound_component.play()
