852be518badbade735bcded36239afcfd805cbfb
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-02-01 20:12:18 +0300

    window mode completed successfully, maybe i will do some changes in the future

--- scenes/ui/options_menu/options_menu.gd
+++ scenes/ui/options_menu/options_menu.gd
@@ -2,6 +2,17 @@ extends CanvasLayer
 
 @onready var window_mode_button: Button = %WindowModeButton
 
+func _ready():
+	update_options()
+
+
+func update_options():
+	if DisplayServer.window_get_mode() \
+	== DisplayServer.WINDOW_MODE_FULLSCREEN:
+		window_mode_button.text = "Fullscreen"
+	else:
+		window_mode_button.text = "Windowed"
+
 
 func _on_window_mode_button_pressed() -> void:
 	#window mode request 
@@ -10,3 +21,4 @@ func _on_window_mode_button_pressed() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else: 
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	update_options()
