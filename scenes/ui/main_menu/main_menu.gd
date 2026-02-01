296b49215454e32b72e1db76952b2f8fcba9a892
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-02-01 20:28:29 +0300

    okay, added sfx and music sliders that control volume of effects and music. nice work(also now button back in options menu works)

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -1,12 +1,15 @@
 extends CanvasLayer
 
+var options_menu_scene = preload("res://scenes/ui/options_menu/options_menu.tscn")
+
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level/floor.tscn")
 
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	var options_instance = options_menu_scene.instantiate()
+	add_child(options_instance)
 
 
 func _on_quit_button_pressed() -> void:
