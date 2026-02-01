921310cfcaf35bdc19e7965bea5bb5f65a427eb4
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-02-01 20:28:29 +0300

    okay, added sfx and music sliders that control volume of effects and music. nice work(also now button back in options menu works)

--- scenes/ui/options_menu/options_menu.gd
+++ scenes/ui/options_menu/options_menu.gd
@@ -1,9 +1,17 @@
 extends CanvasLayer
 
 @onready var window_mode_button: Button = %WindowModeButton
+@onready var music_slider: HSlider = %MusicSlider
+@onready var sfx_slider: HSlider = %SFXSlider
+
 
 func _ready():
 	update_options()
+
+
+func get_volume_percent(bus_index: int):
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
 
 
 func update_options():
@@ -12,6 +20,9 @@ func update_options():
 		window_mode_button.text = "Fullscreen"
 	else:
 		window_mode_button.text = "Windowed"
+	
+	sfx_slider.value = get_volume_percent(2)
+	music_slider.value = get_volume_percent(1)
 
 
 func _on_window_mode_button_pressed() -> void:
@@ -21,4 +32,19 @@ func _on_window_mode_button_pressed() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else: 
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
 	update_options()
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	var volume_db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(2, volume_db)
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	var volume_db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(1, volume_db)
+
+
+func _on_back_button_pressed() -> void:
+	queue_free()
