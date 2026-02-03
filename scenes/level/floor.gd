b3de259ecd1f6d7cccb0eca025875b49ae9e2861
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-02-03 22:11:56 +0300

    i fill everything is ok(i didnt test all cases). added game pause in "escape" and "p". a lot of small details i'm to lazy to explain them, but they are easy

--- scenes/level/floor.gd
+++ scenes/level/floor.gd
@@ -4,6 +4,8 @@ extends Node
 
 @onready var player = $Back/player
 
+var pause_menu_scene = preload("res://scenes/ui/pause_menu/pause_menu.tscn")
+
 func _ready():
 	MusicPlayer.play()
 	player.health_component.died.connect(on_died)
@@ -12,3 +14,8 @@ func on_died():
 	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
 	add_child(end_screen_instance)
 	end_screen_instance.play_jingle()
+
+#встроенная функция для обработки ввода клавиши
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())	
