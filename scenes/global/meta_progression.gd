d754ad73d55a95e81345a38c7f4bb4570f6f2b5d
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-02-04 02:05:56 +0300

    did load and save but it didnt work(

--- scenes/global/meta_progression.gd
+++ scenes/global/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+#var save_path = "D:/учёба/диплом/RealTopDown/saves/game.save"
+var save_path = "user://game.save"
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {}
@@ -8,6 +10,18 @@ var save_data: Dictionary = {
 func _ready() -> void:
 	Global.experience_bottle_collected.connect(on_experience_collected)
 	add_meta_upgrade(load("res://resources/meta_upgrades/experience_drop_chance.tres"))
+	load_file()
+
+func save_file():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(save_data)
+
+
+func load_file():
+	if not FileAccess.file_exists(save_path):
+		return
+	var file = FileAccess.open(save_path, FileAccess.READ)
+	save_data = file.get_var()
 
 
 func add_meta_upgrade(upgrade: MetaUpgrades):
@@ -16,8 +30,10 @@ func add_meta_upgrade(upgrade: MetaUpgrades):
 			"quantity": 0
 		}
 	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
-	print(save_data)
+	#print(save_data)
 
 
 func on_experience_collected(number: float):
 	save_data["meta_upgrade_currency"] == number
+	save_file()
+	print(save_data)
