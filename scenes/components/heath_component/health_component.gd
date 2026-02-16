03efbbac8804239b1e4f6b96329e5857fdaa9717
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-02-16 21:17:23 +0300

    did some changes(rename health_changed to health_decreased in all files and added health_increased). debug was successfull, keep going

--- scenes/components/heath_component/health_component.gd
+++ scenes/components/heath_component/health_component.gd
@@ -3,7 +3,8 @@ extends Node
 class_name HeathComponent
 
 signal died
-signal health_changed
+signal health_decreased
+signal health_increased
 
 @export var max_health: float = 10
 @export var damage_text_scene: PackedScene
@@ -12,21 +13,34 @@ var current_health: float
 
 func _ready() -> void:
 	current_health = max_health
-	
+
+
 func take_damage(damage):
 	var front_layer = get_tree().get_first_node_in_group("front_layer") as Node2D
 	var damage_text_instance = damage_text_scene.instantiate() as Node2D
 	front_layer.add_child(damage_text_instance)
 	damage_text_instance.global_position = owner.global_position
 	damage_text_instance.damage_text(damage)
-	
 	current_health = max(current_health - damage, 0)
-	health_changed.emit()
+	health_decreased.emit()
 	Callable(check_death).call_deferred()
 	#print("current health is ", current_health)
-	
+
+
+func take_heal(heal):
+	var front_layer = get_tree().get_first_node_in_group("front_layer") as Node2D
+	var damage_text_instance = damage_text_scene.instantiate() as Node2D
+	front_layer.add_child(damage_text_instance)
+	damage_text_instance.global_position = owner.global_position
+	damage_text_instance.modulate = Color("726cde")
+	damage_text_instance.damage_text(heal)
+	current_health = min(current_health + heal, max_health)
+	health_increased.emit()
+
+
 func get_health_value():
 	return current_health / max_health
+
 
 func check_death():
 	if current_health <= 0:
