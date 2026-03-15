84e66176f3a6bb7dacbbff384e16058100f9d5f6
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-03-15 22:48:24 +0300

    When the player clicks on e, the quest begins.

--- scenes/game_objects/test_player/test_player.gd
+++ scenes/game_objects/test_player/test_player.gd
@@ -1,18 +1,31 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var arena_time_manager: ArenaTimeManager
+@onready var label: Label = $Label
 
 var player = null
 
 func _ready():
+	label.visible = false
+	label.text = "press e to start"
 	animated_sprite_2d.play("idle")
 
 
 func _process(delta: float) -> void:
 	if player:
-		print("is Player")
+		label.visible = true
+		if Input.is_action_just_pressed("e"):
+			start_game()
 	if !player:
-		print("not player")
+		label.visible = false
+
+
+func start_game():
+	if arena_time_manager: #and not arena_time_manager.is_game_active:
+		print("1")
+		arena_time_manager.start_game()
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
--- scenes/manager/arena_time/arena_time_manager.gd
+++ scenes/manager/arena_time/arena_time_manager.gd
@@ -3,18 +3,32 @@ extends Node
 class_name ArenaTimeManager
 
 @export var end_screen_scene: PackedScene
-@export var game_length:float = 600
+@export var game_length:float = 60
 
 @onready var timer = $Timer
 @onready var difficulty_timer = $DifficultyTimer
 
 signal difficulty_increased(difficulty_level: int) 
+signal game_started()
 
 var difficulty_level: int = 0
+var is_game_active: bool = false
 
 func _ready():
-	timer.start(game_length)
+	add_to_group("arena_time_manager")
+	#timer.start(game_length)
+	process_mode = Node.PROCESS_MODE_DISABLED
 
+
+func start_game():
+	if is_game_active:
+		return
+	is_game_active = true
+	process_mode = Node.PROCESS_MODE_INHERIT
+	timer.start(game_length)
+	difficulty_timer.start()
+	game_started.emit()
+	print("Квест начался")
 
 func gold_to_add():
 	#floor - округление в меньшую сторону, round - в большую
@@ -22,10 +36,12 @@ func gold_to_add():
 
 
 func get_time_elapsed():
-	return game_length - timer.time_left
+	return game_length - timer.time_left if is_game_active else 0
 
 	
 func _on_timer_timeout() -> void:
+	if not is_game_active:
+		return
 	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
 	get_parent().add_child(end_screen_instance)
 	end_screen_instance.change_to_victory()
@@ -33,5 +49,7 @@ func _on_timer_timeout() -> void:
 	end_screen_instance.play_jingle(1)
 
 func _on_difficulty_timer_timeout() -> void:
+	if not is_game_active:
+		return
 	difficulty_level += 1
 	difficulty_increased.emit(difficulty_level)
