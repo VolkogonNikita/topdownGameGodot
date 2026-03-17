443c5bd80a15fef8307e4715ff4ddcbc87049cde
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-03-17 22:06:37 +0300

    concept of mini profile

--- axe.gd
+++ axe.gd
@@ -2,7 +2,7 @@ extends Skill
 class_name AxeSpell
 
 func _init(target) -> void:
-	cooldown = 1.0
+	cooldown = 3.0
 	texture = preload("res://scenes/abilities/axe_ability/weapon_throwing_axe.png")
 	#texture = preload("res://scenes/abilities/anvil_ability/anvil.png")
 	super._init(target)
