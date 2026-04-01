d6dff82c9e3c24b68e402073ef0b7dac8c47af92
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-02 02:09:00 +0300

    begin of dungeon

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -4,7 +4,7 @@ var options_menu_scene = preload("res://scenes/ui/options_menu/options_menu.tscn
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level/floor.tscn")
+	get_tree().change_scene_to_file("res://scenes/level/world/world.tscn")
 
 
 func _on_options_button_pressed() -> void:
