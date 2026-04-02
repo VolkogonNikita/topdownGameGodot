85ff2ca3c18fe0ec7d3b4dca7576c94aa8606bc3
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-03 01:26:46 +0300

    pointless new assets, i will remove them. added switch between world and floor(dungeon) scenes

--- scenes/level/floor.gd
+++ scenes/level/floor.gd
@@ -10,6 +10,14 @@ var pause_menu_scene = preload("res://scenes/ui/pause_menu/pause_menu.tscn")
 func _ready():
 	MusicPlayer.play()
 	player.health_component.died.connect(on_died)
+	#player = null
+
+
+func _process(delta: float) -> void:
+	if player:
+		if Input.is_action_just_released("action"):
+			await get_tree().create_timer(0.5).timeout
+			get_tree().change_scene_to_file("res://scenes/level/world/world.tscn")
 
 func on_died():
 	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
@@ -21,3 +29,14 @@ func on_died():
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		add_child(pause_menu_scene.instantiate())	
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("exit")
+		player = body
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player = null
