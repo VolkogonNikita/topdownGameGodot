79a9d6862e051233eabd710ef45377793fd56759
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-07 21:33:49 +0300

    inventory ui part 1

--- scenes/ui/final_ui/inv_ui.gd
+++ scenes/ui/final_ui/inv_ui.gd
@@ -1,6 +1,19 @@
 extends Control
 
+@onready var inv: Inv = preload("res://scenes/ui/final_ui/items/players_inventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open: bool = false
+
+func _ready() -> void:
+	update_slots()
+	close()
+
+
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("b"):
@@ -9,10 +22,12 @@ func _process(delta: float) -> void:
 
 
 func open():
-	is_open = true
+	print("open")
+	self.is_open = true
 	visible = true
 
 
 func close():
+	print("close")
 	is_open = false
 	visible = false
