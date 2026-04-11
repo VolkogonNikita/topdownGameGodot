87c9791a7b2297afd9e6714a94caab0d71afd859
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-11 15:24:29 +0300

    add minimap in dungeon and fix trap collision

---
+++ scenes/level/sub_viewport.gd
@@ -0,0 +1,10 @@
+extends SubViewport
+
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var player: Player = $"../../../Back/player"
+
+
+func _physics_process(delta: float) -> void:
+	camera_2d.global_position = player.global_position
+	sprite_2d.global_position = player.global_position
