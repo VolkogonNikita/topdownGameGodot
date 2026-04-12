2bc20e20072bec76b674efeea3cb7b01898d207f
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-12 15:30:07 +0300

    added puddles

--- scenes/level/world/world.gd
+++ scenes/level/world/world.gd
@@ -5,6 +5,14 @@ extends Node2D
 
 var player = null
 var pause_menu_scene = preload("res://scenes/ui/pause_menu/pause_menu.tscn")
+var puddle_scene = preload("res://scenes/level/world/puddle.tscn")
+
+@export var puddle_lifetime: float = 10   
+@export var spawn_delay: float = 0.03     
+@export var puddle_max_count: int = 1000   
+
+var puddle_count = 0
+var puddle_timer: float = 0.0
 
 func _ready() -> void:
 	MusicPlayer.play()
@@ -12,10 +20,23 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	animation_player.play("day-night")
+	spawn_puddle()
 	if player: 
 		if Input.is_action_just_pressed("action"):
 			await get_tree().create_timer(0.5).timeout
 			get_tree().change_scene_to_file("res://scenes/level/floor.tscn")
+
+
+func spawn_puddle():
+	var puddle = puddle_scene.instantiate()
+	puddle.position = Vector2(randf_range(-1000, 2000), randf_range(-1000,2000))
+	add_child(puddle)
+	puddle_count += 1
+	var timer = get_tree().create_timer(puddle_lifetime)
+	timer.timeout.connect(func(): 
+		if is_instance_valid(puddle): 
+			puddle.queue_free() 
+			puddle_count -= 1 )
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
