572728b7e318fb7b918358d85119a2b87ce87a52
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-15 21:58:51 +0300

    download new assets for skiils, also added heal spell

--- scenes/ui/final_ui/inv_ui.gd
+++ scenes/ui/final_ui/inv_ui.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @onready var inv: Inv = preload("res://scenes/ui/final_ui/items/players_inventory.tres")
-#@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 @export var health_component: HealthComponent
 @export var stamina_component: StaminaComponent
 @export var exp_component: ExperienceManager
@@ -9,16 +8,14 @@ extends Control
 var is_open: bool = false
 
 func _ready() -> void:
-	#inv.update.connect(on_update_slots)
-	#fupdate_slots()
 	close()
 
 
 func _process(delta: float) -> void:
 	$HealthValueLabel.text = str(health_component.current_health) + "/" + str(health_component.max_health)
-	$StaminaValueLabel.text = str(stamina_component.current_stamina) + "/" + str(stamina_component.max_stamina)
+	$StaminaValueLabel.text = "%.0f/%.0f" % [stamina_component.current_stamina, stamina_component.max_stamina]
 	$ExpValueLabel.text = str(exp_component.current_experience) + "/" + str(exp_component.target_experience)
-
+	$LevelLabel.text = str(exp_component.current_level) + " lvl"
 	if Input.is_action_just_pressed("tab"):
 		if is_open: close()
 		else: open()
@@ -34,8 +31,3 @@ func close():
 	print("close")
 	is_open = false
 	visible = false
-
-
-#func on_update_slots():
-	#for i in range(min(inv.slots.size(), slots.size())):
-		#slots[i].update(inv.slots[i])
