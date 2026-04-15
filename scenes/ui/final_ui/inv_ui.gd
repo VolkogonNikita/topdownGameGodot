4b430f508e5e7a44145f0d0aae40dd2b69e33544
Volkogon Nikita Igorevich <volkogonnikita9@gmail.com>
2026-04-15 17:07:41 +0300

    font fix. sprite2d for player image, labels for player's name, stamina, health and exp

--- scenes/ui/final_ui/inv_ui.gd
+++ scenes/ui/final_ui/inv_ui.gd
@@ -1,17 +1,24 @@
 extends Control
 
 @onready var inv: Inv = preload("res://scenes/ui/final_ui/items/players_inventory.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+#@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@export var health_component: HealthComponent
+@export var stamina_component: StaminaComponent
+@export var exp_component: ExperienceManager
 
 var is_open: bool = false
 
 func _ready() -> void:
-	inv.update.connect(on_update_slots)
+	#inv.update.connect(on_update_slots)
 	#fupdate_slots()
 	close()
 
 
 func _process(delta: float) -> void:
+	$HealthValueLabel.text = str(health_component.current_health) + "/" + str(health_component.max_health)
+	$StaminaValueLabel.text = str(stamina_component.current_stamina) + "/" + str(stamina_component.max_stamina)
+	$ExpValueLabel.text = str(exp_component.current_experience) + "/" + str(exp_component.target_experience)
+
 	if Input.is_action_just_pressed("tab"):
 		if is_open: close()
 		else: open()
@@ -29,6 +36,6 @@ func close():
 	visible = false
 
 
-func on_update_slots():
-	for i in range(min(inv.slots.size(), slots.size())):
-		slots[i].update(inv.slots[i])
+#func on_update_slots():
+	#for i in range(min(inv.slots.size(), slots.size())):
+		#slots[i].update(inv.slots[i])
